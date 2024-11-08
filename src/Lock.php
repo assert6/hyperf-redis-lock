aeb57ea80d40dce7285e666a9adf956062ffb14b
张城铭 <z@hyperf.io>
2024-11-08 14:16:39 +0800

    Update Lock.php

--- src/Lock.php
+++ src/Lock.php
@@ -2,8 +2,8 @@
 
 namespace Lysice\HyperfRedisLock;
 
-use Hyperf\Utils\Str;
-use Hyperf\Utils\InteractsWithTime;
+use Hyperf\Stringable\Str;
+use Hyperf\Support\InteractsWithTime;
 
 abstract class Lock implements LockContract
 {
